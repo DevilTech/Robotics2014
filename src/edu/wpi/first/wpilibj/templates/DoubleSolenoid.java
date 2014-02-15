c2bdc774d0ee03be518a4fd177d31aad94a7a30a
Jeremy <jweed262@gmail.com>
2014-02-15 10:45:35 -0500

    added class

---
+++ src/edu/wpi/first/wpilibj/templates/DoubleSolenoid.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import Competition.Wiring;
+import edu.wpi.first.wpilibj.Solenoid;
+
+/**
+ *
+ * @author jeremy
+ */
+public class DoubleSolenoid {
+
+    Solenoid one;
+    Solenoid two;
+    int onePasses = 0;
+    int twoPasses = 0;
+    boolean oneStatus = false;
+    boolean twoStatus = false;
+
+    public DoubleSolenoid(int valveOne, int valveTwo) {
+        one = new Solenoid(valveOne);
+        two = new Solenoid(valveTwo);
+
+    }
+
+    public void valveOne(boolean on) {
+        if (on) {
+            oneStatus = true;
+            if (onePasses < Wiring.MAX_SOLENOID_PASSES) {
+                one.set(true);
+                onePasses++;
+            } else {
+                one.set(false);
+
+            }
+        } else {
+            oneStatus = false;
+            one.set(false);
+            onePasses = 0;
+        }
+
+    }
+    public void valveTwo(boolean on) {
+        if (on) {
+            twoStatus = true;
+            if (twoPasses < Wiring.MAX_SOLENOID_PASSES) {
+                two.set(true);
+                twoPasses++;
+            } else {
+                two.set(false);
+
+            }
+        } else {
+            twoStatus = false;
+            two.set(false);
+            twoPasses = 0;
+        }
+
+    }
+    
+   public void reverse(){
+       valveOne(!oneStatus);
+       valveTwo(!twoStatus);
+   } 
+   
+   public void set(boolean oneOn){
+       valveOne(oneOn);
+       valveTwo(!oneOn);
+   }
+    
+}
